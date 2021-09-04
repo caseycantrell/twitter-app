9821d78fa035323ebcd3d5383f9a85847b979f9c
Casey <cantrellcasey@gmail.com>
2021-09-04 18:43:07 -0500

    adds controller actions

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,34 @@
 class TweetsController < ApplicationController
+
+  def index
+    tweets = Tweets.all
+    render json: tweets
+  end
+
+  def show
+    tweet = Tweet.find(params[:id])
+    render json: tweet
+  end
+
+  def create
+    tweet = Tweet.create(
+      user_id: params[:user_id],
+      body: params[:body],
+      date_posted: params[:date_posted]
+    )
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.user_id = params[:user_id] || tweet.user_id
+    tweet.body = params[:body] || tweet.body
+    tweet.date_posted = params[:date_posted] || tweet.date_posted
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    render json: {message: "That thing has been obliterated."}
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+  
+  def index
+    users = Users.all
+    render json: users
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def create
+    user = User.create(
+      username: params[:username],
+      url: params[:url]
+    )
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.username = params[:user_id] || user.username
+    user.url = params[:body] || user.url
+  end
+
+  def destroy
+    user = Tweet.find(params[:id])
+    user.destroy
+    render json: {message: "That thing has been obliterated."}
+  end
+
 end
