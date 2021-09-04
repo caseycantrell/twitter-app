6501fbef67c3c8c7ae861ccfebf7e3a1a0840a76
Casey <cantrellcasey@gmail.com>
2021-09-04 18:35:10 -0500

    adds restful routes, http requests, and controller generation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/users" => "users#index"
+  get "/users/:id" => "users#show"
+  post "/users" => "users#create"
+  patch "/users/:id" => "users#update"
+  delete "/users/:id" => "users#destroy"
+
+  get "/tweets" => "tweets#index"
+  get "/tweets/:id" => "tweets#show"
+  post "/tweets" => "tweets#create"
+  patch "/tweets/:id" => "tweets#update"
+  delete "/tweets/:id" => "tweets#destroy"
+
 end
